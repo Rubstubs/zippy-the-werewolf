57ab7357470026ce471ea36d8ab94209a4a3d5c0
ruben <rubencarlsen@gmail.com>
2022-03-15 17:06:28 +0100

    init dev branch

--- src/main/kotlin/no/rubstubs/filestorage/config/AppConfig.kt
+++ src/main/kotlin/no/rubstubs/filestorage/config/AppConfig.kt
@@ -3,7 +3,7 @@ import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 
 /**
- * Setting up for running something on a separate thread
+ * Setting up for running something on a separate  thread
  */
 @Configuration
 @ComponentScan(basePackages = ["no.rubstubs.filestorage.thread"])
