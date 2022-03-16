65003ebdf8bc74a32c0afb151b21d53ace6e015e
Dontosh <me@torsteinstrand.com>
2022-03-16 15:01:45 +0100

    React app up and running. Basic design with werewolf and simple animation file being added. Everything works with the backend.

---
+++ client/components/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import {MainPage} from "./MainPage";
+
+export function App() {
+    return(
+        <div>
+        <MainPage />
+    </div>);
+}
---
+++ client/components/MainPage.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import zippy from "../public/zippy.PNG";
+
+export function MainPage() {
+    return(
+        <div>
+            <h1 id="zippy-title">Give Zippy some files, if you dare!</h1>
+            <div className="zippy-img-container">
+                <img id="zippy" src={zippy} alt="zippy, the werewolf"/>
+                <i id="file-icon" className="bi bi-card-checklist"/>
+            </div>
+            <div>
+                <form className="form" method="POST" action="/" encType="multipart/form-data" id="fileUploadForm">
+                    <div className="choose-file-container">
+                        <input onChange={() => document.getElementById("file-icon").classList.toggle("clicked")} type="file" placeholder="Upload and zip your file" multiple name="files"/>
+                    </div>
+                    {/*<label htmlFor="email">Your email</label>*/}
+                    <input placeholder="Your email" type="email" name="email"/>
+                    <button>Go forth</button>
+                </form>
+            </div>
+        </div>
+    );
+}
